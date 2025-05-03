5bacd02e318b8aecd2add23fbf372266c74c04c2
Nadim Searight <ns241@st-andrews.ac.uk>
2025-05-03 14:18:05 +0100

    added mappings for lsp definitions

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "gd", function() vim.lsp.buf.definition() end)
+map("n", "gD", function() vim.lsp.buf.declaration() end)
+map("n", "gt", function() vim.lsp.buf.type_definition() end)
